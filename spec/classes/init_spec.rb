539befa879ccaaf28a37c0d246d68485e4bf853c
Dana Parker <dwatkins-parker@keywcorp.com>
2015-10-27 22:37:26 +0000

    (SIMP-574) Updated spec, added acceptance tests
    Updated spec and acceptance tests.
    Updated the metadata.json file.
    
    SIMP-574 #close updated acceptance test
    
    Change-Id: Ib7a7831e282b1aec4e272a5d48cb563a17c39457

--- spec/classes/init_spec.rb
+++ spec/classes/init_spec.rb
@@ -1,9 +1,29 @@
 require 'spec_helper'
 
 describe 'acpid' do
-  it { is_expected.to create_class('acpid') }
+  context 'supported operating systems' do
+    on_supported_os.each do |os, facts|
+      let(:facts) do
+        facts
+      end
 
-  it { is_expected.to compile.with_all_deps }
-  it { is_expected.to create_package('acpid') }
-  it { is_expected.to create_service('acpid').that_requires('Package[acpid]') }
+      context "on #{os}" do
+        it { is_expected.to compile.with_all_deps }
+        it { is_expected.to create_class('acpid') }
+
+        context 'base' do
+          it { is_expected.to contain_package('acpid').with( :ensure => 'latest')}
+          it { is_expected.to contain_service('acpid').that_requires('Package[acpid]') }
+          it { is_expected.to contain_service('acpid').with({
+            :ensure     => 'running',
+            :enable     => true,
+            :hasstatus  => true,
+            :hasrestart => true,
+            :start      => '/sbin/service haldaemon stop; /sbin/service acpid start; /sbin/service haldaemon start',
+            })
+          }
+        end
+      end
+    end
+  end
 end
