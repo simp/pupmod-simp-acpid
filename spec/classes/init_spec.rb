7d5ffb7159afaaf8a27dfc9d08c43540898121d5
Chris Tessmer <chris.tessmer@onyxpoint.com>
2015-12-07 11:40:10 -0500

    (SIMP-667) Normalize common module assets
    This commit synchronizes all common static module assets with current SIMP
    module standards.
    
    Also:
    - created metadata.json
    - updated rspec tests to the new `expect` syntax
    
    SIMP-667 #comment updated `pupmod-simp-acpid`

--- spec/classes/init_spec.rb
+++ spec/classes/init_spec.rb
@@ -1,9 +1,9 @@
 require 'spec_helper'
 
 describe 'acpid' do
-  it { should create_class('acpid') }
+  it { is_expected.to create_class('acpid') }
 
-  it { should compile.with_all_deps }
-  it { should create_package('acpid') }
-  it { should create_service('acpid').that_requires('Package[acpid]') }
+  it { is_expected.to compile.with_all_deps }
+  it { is_expected.to create_package('acpid') }
+  it { is_expected.to create_service('acpid').that_requires('Package[acpid]') }
 end
