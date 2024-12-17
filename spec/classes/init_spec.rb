1251970ac8529ab2e475d304b1fcb8131388723e
Steven Pritchard <steve@sicura.us>
2024-12-17 10:53:25 -0600

    Additional cleanup

--- spec/classes/init_spec.rb
+++ spec/classes/init_spec.rb
@@ -1,10 +1,8 @@
 require 'spec_helper'
 
 describe 'acpid' do
-  on_supported_os.each do |os, facts|
-    let(:facts) do
-      facts
-    end
+  on_supported_os.each do |os, os_facts|
+    let(:facts) { os_facts }
 
     context "on #{os}" do
       it { is_expected.to compile.with_all_deps }
@@ -13,14 +11,15 @@ describe 'acpid' do
       context 'base' do
         it { is_expected.to contain_package('acpid').with(ensure: 'installed') }
         it { is_expected.to contain_service('acpid').that_requires('Package[acpid]') }
-        it {
-          is_expected.to contain_service('acpid').with({
-                                                         ensure: 'running',
-          enable: true,
-          hasstatus: true,
-          hasrestart: true
-                                                       })
-        }
+        it do
+          is_expected.to contain_service('acpid')
+            .with(
+              ensure: 'running',
+              enable: true,
+              hasstatus: true,
+              hasrestart: true,
+            )
+        end
       end
     end
   end
