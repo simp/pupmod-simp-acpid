69900ff365b417cd5678677e634adf0705a99422
Steven Pritchard <steve@sicura.us>
2024-12-17 13:34:26 -0600

    Clean up for rubocop (#79)
    * Clean up for rubocop
    
    * Additional cleanup

--- spec/classes/init_spec.rb
+++ spec/classes/init_spec.rb
@@ -1,25 +1,25 @@
 require 'spec_helper'
 
 describe 'acpid' do
-  on_supported_os.each do |os, facts|
-    let(:facts) do
-      facts
-    end
+  on_supported_os.each do |os, os_facts|
+    let(:facts) { os_facts }
 
     context "on #{os}" do
       it { is_expected.to compile.with_all_deps }
       it { is_expected.to create_class('acpid') }
 
       context 'base' do
-        it { is_expected.to contain_package('acpid').with( :ensure => 'installed')}
+        it { is_expected.to contain_package('acpid').with(ensure: 'installed') }
         it { is_expected.to contain_service('acpid').that_requires('Package[acpid]') }
-        it { is_expected.to contain_service('acpid').with({
-          :ensure     => 'running',
-          :enable     => true,
-          :hasstatus  => true,
-          :hasrestart => true
-          })
-        }
+        it do
+          is_expected.to contain_service('acpid')
+            .with(
+              ensure: 'running',
+              enable: true,
+              hasstatus: true,
+              hasrestart: true,
+            )
+        end
       end
     end
   end
